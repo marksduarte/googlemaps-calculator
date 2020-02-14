bf962c9293dd4211032a7b85fd455c1ac4a810c0
marksduarte <djm@rk$87>
2020-02-14 17:34:06 -0300

    Consultando o valor médio do combustível.

--- js/background.js
+++ js/background.js
@@ -1,18 +1,27 @@
-chrome.browserAction.onClicked.addListener(function(tab) {    
-    chrome.tabs.update({
-            "url": "https://www.google.com.br/maps/dir/Setor+Comercial+Norte+Q+2+Liberty+Mall+Shoping+-+Asa+Norte,+Bras%C3%ADlia+-+DF//@-15.7867346,-47.9196024,13z/data=!4m9!4m8!1m5!1m1!1s0x935a3afdad78c1d5:0x6d428966d18e37a9!2m2!1d-47.884583!2d-15.78682!1m0!3e0?hl=pt-BR",
-            "active": true
-    });    
+/**
+ *  Background Script
+ * @author Marks Duarte <marksduarte@gmail.com>
+ *
+ */
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({
+    url:
+      "https://www.google.com.br/maps/dir/Setor+Comercial+Norte+Q+2+Liberty+Mall" +
+      "+Shoping+-+Asa+Norte,+Bras%C3%ADlia+-+DF//@-15.7867346,-47.9196024,13z" +
+      "/data=!4m9!4m8!1m5!1m1!1s0x935a3afdad78c1d5:0x6d428966d18e37a9!2m2!1d-47.884583!2d-15.78682!1m0!3e0?hl=pt-BR",
+    active: true
+  });
 });
 
 /**
  * Listener to manipulate windows and tabs
  */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request.message === "create_new_window") {
-        createNewWindow(request.report);
-    } 
-    return true;
+  if (request.message === "create_new_window") {
+    createNewWindow(request.report);
+  }
+  return true;
 });
 
 /**
@@ -20,13 +29,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
  * @param report
  */
 function createNewWindow(report) {
-    var targetId = null;
-    chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
-        if(tabId !== targetId || changedProps.status !== "complete") return;
-        chrome.runtime.sendMessage({message: "load_new_window", report: report});
-    });
-    var newWindowUrl = chrome.extension.getURL('html/report.html');
-    chrome.windows.create({url: newWindowUrl, type: "popup"}, function(window) {
-        targetId = window.tabs[0].id;
-    });
+  var targetId = null;
+  chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+    if (tabId !== targetId || changedProps.status !== "complete") return;
+    chrome.runtime.sendMessage({ message: "load_new_window", report: report });
+  });
+  var newWindowUrl = chrome.extension.getURL("html/report.html");
+  chrome.windows.create({ url: newWindowUrl, type: "popup" }, function(window) {
+    targetId = window.tabs[0].id;
+  });
 }
--- js/content.js
+++ js/content.js
@@ -1,131 +1,187 @@
 /**
  *  Content Script
  * @author Marks Duarte <marksduarte@gmail.com>
- * 
+ *
  */
 
-$(function(){
-    showApp();
+var URL_DADOS_ANP =
+  "http://www.anp.gov.br/images/Precos/Mensal2013/MENSAL_ESTADOS-DESDE_Jan2013.xlsx";
+var URL_GITHUB =
+  "https://marksduarte.github.io/resources/googlemaps-calculator.json";
+var vlLitro;
+
+$(function () {
+  showApp();
+  getValorMedioGasolina();
 });
 
-function showApp() {    
-    if(document.title.indexOf("Google") !== -1) {       
-        var request = new XMLHttpRequest();
-        request.open('GET', chrome.extension.getURL('html/popup.html'), true);
-        request.send(null);
-        request.onload  = function () {
-            if(request.readyState === 4 && request.status === 200) {
-                var type = request.getResponseHeader('Content-Type');
-                if(type.indexOf("text") !== 1) {
-                    let divExt = document.createElement('div');                             
-                    document.body.appendChild(divExt);    
-                    document.getElementById('app-container').style.marginTop = '65px';
+function showApp() {
+  if (document.title.indexOf("Google") !== -1) {
+    var request = new XMLHttpRequest();
+    request.open("GET", chrome.extension.getURL("html/popup.html"), true);
+    request.send(null);
+    request.onload = function () {
+      if (request.readyState === 4 && request.status === 200) {
+        var type = request.getResponseHeader("Content-Type");
+        if (type.indexOf("text") !== 1) {
+          let divExt = document.createElement("div");
+          document.body.appendChild(divExt);
+          document.getElementById("app-container").style.marginTop = "65px";
 
-                    shadowRoot = divExt.attachShadow({mode: 'open'});
-                    shadowRoot.innerHTML = request.responseText;                    
+          shadowRoot = divExt.attachShadow({ mode: "open" });
+          shadowRoot.innerHTML = request.responseText;
 
-                    shadowRoot.querySelector('#btnClose').addEventListener('click', onBtnCloseClick);
-                    shadowRoot.querySelector('#btnCalculate').addEventListener('click', onBtnCalculateClick);
-                    shadowRoot.querySelector('#btnReport').addEventListener('click', onBtnReportClick);
-                    shadowRoot.querySelector('#btnClose a img').src = chrome.extension.getURL("img/close.svg");
-                    shadowRoot.querySelector('#img-map').src = chrome.extension.getURL("img/map.svg");
-                    shadowRoot.querySelector('#img-fuel').src = chrome.extension.getURL("img/fuel.svg");
-                    shadowRoot.querySelector('#img-refund').src = chrome.extension.getURL("img/refund.svg");
-                    
-                    include(chrome.extension.getURL("css/bootstrap.min.css"));
-                    include(chrome.extension.getURL("css/bootstrap-datepicker.standalone.min.css"));  
-                    include(chrome.extension.getURL("css/popup.css"));                                                         
-                }
-            }
-        }                                    
-    }
+          shadowRoot
+            .querySelector("#btnClose")
+            .addEventListener("click", onBtnCloseClick);
+          shadowRoot
+            .querySelector("#btnCalculate")
+            .addEventListener("click", onBtnCalculateClick);
+          shadowRoot
+            .querySelector("#btnReport")
+            .addEventListener("click", onBtnReportClick);
+          shadowRoot.querySelector(
+            "#btnClose a img"
+          ).src = chrome.extension.getURL("img/close.svg");
+          shadowRoot.querySelector("#img-map").src = chrome.extension.getURL(
+            "img/map.svg"
+          );
+          shadowRoot.querySelector("#img-fuel").src = chrome.extension.getURL(
+            "img/fuel.svg"
+          );
+          shadowRoot.querySelector("#img-refund").src = chrome.extension.getURL(
+            "img/refund.svg"
+          );
+
+          include(chrome.extension.getURL("css/bootstrap.min.css"));
+          include(
+            chrome.extension.getURL(
+              "css/bootstrap-datepicker.standalone.min.css"
+            )
+          );
+          include(chrome.extension.getURL("css/popup.css"));
+        }
+      }
+    };
+  }
 }
 
 function include(path) {
-    let el;
-    if(path.endsWith('.css')) {
-        el = document.createElement('link');
-        el.rel = 'stylesheet';
-        el.href = path;        
-    }
-    if(path.endsWith('.js')) {
-        el = document.createElement('script');
-        el.type = 'text/javascript';
-        el.src = path;        
-    }    
-    shadowRoot.appendChild(el);
+  let el;
+  if (path.endsWith(".css")) {
+    el = document.createElement("link");
+    el.rel = "stylesheet";
+    el.href = path;
+  }
+  if (path.endsWith(".js")) {
+    el = document.createElement("script");
+    el.type = "text/javascript";
+    el.src = path;
+  }
+  shadowRoot.appendChild(el);
 }
 
-function turnOffEvents() {
-    // Usar $.off() para desativar o evento dos inputs de destino.
+function onBtnCalculateClick() {
+  if (getKmtotal() <= 0) return;
+  report = {};
+  report.distancia = getKmtotal();
+  report.trajetos = getTrajetos().slice();
+  report.kmMediaAno = 12000;
+  report.kmLitro = 10;
+  report.vlLitro = Number.parseFloat(vlLitro); // 01/2020
+  report.combustivel = (report.distancia / report.kmLitro).toFixed(2);
+  report.consumo = report.vlLitro / report.kmLitro;
+  report.vlFipe = 28935; //Grand Siena ATTRAC. 1.4 EVO F.Flex 8V
+  report.vlSeguro = 120;
+  report.vlLicenciamento = 100;
+  report.vlIpva = report.vlFipe * 0.03;
+  report.vlIpvaSeguroLicenciamento =
+    (report.vlIpva + report.vlSeguro + report.vlLicenciamento) /
+    report.kmMediaAno;
+  report.depreciacao = (0.15 * report.vlFipe) / report.kmMediaAno;
+  report.vlRevisao = ((report.kmMediaAno / 10000) * 500) / report.kmMediaAno; // R$ 500 valor médio da revisão
+  report.vlLitroReembolso =
+    report.vlLitro +
+    (report.consumo +
+      report.vlIpvaSeguroLicenciamento +
+      report.depreciacao +
+      report.vlRevisao);
+  report.vlReembolso = (
+    report.combustivel * report.vlLitroReembolso
+  ).toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+
+  shadowRoot.querySelector("#distancia").value = report.distancia + " km";
+  shadowRoot.querySelector("#combustivel").value = report.combustivel + " lts";
+  shadowRoot.querySelector("#reembolso").value = report.vlReembolso;
 }
 
-function onBtnCalculateClick() {          
-    if(getKmtotal() <= 0) return;
-    report = {};  
-    report.distancia = getKmtotal();
-    report.trajetos = getTrajetos().slice();    
-    report.kmMediaAno = 12000;    
-    report.kmLitro = 10;
-    report.vlLitro = 4.29;
-    report.combustivel = (report.distancia / report.kmLitro).toFixed(2);
-    report.consumo = report.vlLitro / report.kmLitro;
-    report.vlFipe = 28935; //Grand Siena ATTRAC. 1.4 EVO F.Flex 8V
-    report.vlSeguro = 120;
-    report.vlLicenciamento = 100;
-    report.vlIpva = report.vlFipe * 0.03;
-    report.vlIpvaSeguroLicenciamento = ((report.vlIpva + report.vlSeguro + report.vlLicenciamento) / report.kmMediaAno);
-    report.depreciacao = (0.15 * report.vlFipe) / report.kmMediaAno;
-    report.vlRevisao = ((report.kmMediaAno / 10000) * 500) / report.kmMediaAno; // R$ 500 valor médio da revisão
-    report.vlLitroReembolso = report.vlLitro + (report.consumo + report.vlIpvaSeguroLicenciamento + report.depreciacao + report.vlRevisao); 
-    report.vlReembolso = (report.combustivel * report.vlLitroReembolso).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-        
-    shadowRoot.querySelector('#distancia').value = report.distancia + ' km';    
-    shadowRoot.querySelector('#combustivel').value = report.combustivel + ' lts';
-    shadowRoot.querySelector('#reembolso').value = report.vlReembolso;      
-}
-
-function onBtnReportClick() {  
-    try {
-        chrome.runtime.sendMessage({
-            message: "create_new_window", 
-            report: report
-        });
-    } catch (error) {
-        console.log('É necessário calcular um trajeto antes de emitir o relatório.')
-    }    
+function onBtnReportClick() {
+  try {
+    chrome.runtime.sendMessage({
+      message: "create_new_window",
+      report: report
+    });
+  } catch (error) {
+    console.log(
+      "É necessário calcular um trajeto antes de emitir o relatório."
+    );
+  }
 }
 
 function onBtnCloseClick() {
-    var app = shadowRoot.getElementById('calculadora-google-maps');    
-    app.setAttribute('style', 'display: none');
-    document.getElementById('app-container').style.marginTop = '0';
+  var app = shadowRoot.getElementById("calculadora-google-maps");
+  app.setAttribute("style", "display: none");
+  document.getElementById("app-container").style.marginTop = "0";
 }
 
-function getKmtotal(){
-    try {
-        let elementsByClassName = 
-        document.getElementsByClassName('section-directions-trip-distance section-directions-trip-secondary-text');
-        let txt = elementsByClassName[0].innerText;
-        if(txt !== "" && txt.endsWith("km")) return textToFloat(txt);        
-    } catch (error) {
-        console.log('Defina um trajeto antes de calcular.')
-        return 0;
+function getKmtotal() {
+  try {
+    let elementsByClassName = document.getElementsByClassName(
+      "section-directions-trip-distance section-directions-trip-secondary-text"
+    );
+    let txt = elementsByClassName[0].innerText;
+    if (txt !== "" && txt.endsWith("km")) return textToFloat(txt);
+  } catch (error) {
+    console.log("Defina um trajeto antes de calcular.");
+    return 0;
+  }
+}
+
+function getTrajetos() {
+  let searchboxContainer = document.getElementsByClassName(
+    "widget-directions-searchboxes"
+  )[0];
+  if (searchboxContainer != null) {
+    var arr = [];
+    for (
+      let i = 0;
+      i < searchboxContainer.getElementsByTagName("input").length;
+      i++
+    ) {
+      arr.push(
+        searchboxContainer.getElementsByTagName("input")[i].value.split(",")[0]
+      );
     }
+    return arr;
+  }
 }
 
-function getTrajetos(){
-    let searchboxContainer = document.getElementsByClassName('widget-directions-searchboxes')[0];
-    if(searchboxContainer != null) {
-        var arr = [];
-        for(let i = 0; i < searchboxContainer.getElementsByTagName('input').length; i++) {            
-            arr.push(searchboxContainer.getElementsByTagName('input')[i].value.split(',')[0]);
-        }
-        return arr;
-    }   
+function getValorMedioGasolina() {
+  var request = new XMLHttpRequest();
+  request.responseType = "json";
+  request.open("GET", URL_GITHUB, true);
+  request.send();
+  request.onload = function () {
+    vlLitro = (request.response != null) ? request.response.valor : 4.6;
+  };
 }
 
 function textToFloat(txt) {
-    txt = String(txt);
-    return parseFloat(txt.replace(".","").replace(",",".").trim());
+  txt = String(txt);
+  return parseFloat(
+    txt
+      .replace(".", "")
+      .replace(",", ".")
+      .trim()
+  );
 }
